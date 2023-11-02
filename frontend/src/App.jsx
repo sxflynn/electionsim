5e54e9f77e5b069bef74964b8bc869013e18353f
sxflynn <stephenflynn@me.com>
2023-11-02 03:05:14 -0400

    trying

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -80,7 +80,7 @@ function App() {
             console.log('multi election');
         }
 
-          const response = await fetch(`https://fastapi-election.fly.dev${endpoint}`, {
+        const response = await fetch(`https://fastapi-election.fly.dev${endpoint}`, {
               method: 'POST',
               headers: {
                   'Content-Type': 'application/json'
