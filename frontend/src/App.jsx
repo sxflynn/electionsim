a03d12e80d41bb360169a9fcbd37617654f6e404
Stephen Flynn <sxflynn@users.noreply.github.com>
2023-11-02 02:47:01 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -80,7 +80,7 @@ function App() {
             console.log('multi election');
         }
 
-          const response = await fetch(`http://10.0.0.132:8000${endpoint}`, {
+          const response = await fetch(`https://fastapi-election.fly.dev${endpoint}`, {
               method: 'POST',
               headers: {
                   'Content-Type': 'application/json'
